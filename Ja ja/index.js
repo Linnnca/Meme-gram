067986e6be9135e3e878ac6df1ec6c6b58206bce
Linnnca <diegolincris638@gmail.com>
2024-07-16 00:04:18 -0300

    Add files via upload

---
+++ Ja ja/index.js
@@ -0,0 +1,9 @@
+function redirecionarParaBurgerKing() {
+    window.location.href = "https://www.burgerking.com.br/";
+  }
+ 
+ 
+  function abrirBurgerKingEmNovaAba() {
+    window.open("https://www.burgerking.com.br/", "_blank");
+  }
+  
