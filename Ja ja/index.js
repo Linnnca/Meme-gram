f5e19b6a849b3c04c6814f01e4ba48dfc3fdb32d
Linnnca <diegolincris638@gmail.com>
2024-07-16 15:55:54 -0300

    Update index.js

--- Ja ja/index.js
+++ Ja ja/index.js
@@ -1,9 +1,16 @@
 function redirecionarParaBurgerKing() {
-    window.location.href = "https://www.burgerking.com.br/";
-  }
- 
- 
-  function abrirBurgerKingEmNovaAba() {
-    window.open("https://www.burgerking.com.br/", "_blank");
-  }
-  
+  window.location.href = "https://www.burgerking.com.br/";
+}
+
+
+function abrirBurgerKingEmNovaAba() {
+  window.open("https://www.burgerking.com.br/", "_blank");
+}
+
+function rediinsta() {
+  window.location.href = "https://www.instagram.com/diego_.lincoln?igsh=enB1eTY0dGZma3Yx";
+
+}
+function abririnsta(){
+  window.open("https://www.instagram.com/diego_.lincoln?igsh=enB1eTY0dGZma3Yx","_blank")
+}
